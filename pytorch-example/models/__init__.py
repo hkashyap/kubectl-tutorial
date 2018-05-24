553016241f8a0f1d6ab96d4d0a1c4982987a3dbc
Hirak J Kashyap <kashyaph@uci.edu>
2018-05-24 00:36:17 -0700

    add dispnet pytorch 3

---
+++ pytorch-example/models/__init__.py
@@ -0,0 +1,1 @@
+from .DispNetS import DispNetS
---
+++ pytorch-example/models/download_dispnet_model.sh
@@ -0,0 +1,3 @@
+URL=https://drive.google.com/open?id=1SOyTw_XDzirdf-tSz_TV7HjAG4wWB3nr
+OUTPUT_FILE=./models/dispnet_model_best.pth.tar
+wget -N $URL -O $OUTPUT_FILE
